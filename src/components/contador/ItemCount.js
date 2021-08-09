8f1cc19f9e88bde78a627030dddbbdbdaff2e38c
unknown <ivan.ramunda@gmail.com>
2021-08-09 20:41:11 -0300

    sigue sin salir

--- src/components/contador/ItemCount.js
+++ src/components/contador/ItemCount.js
@@ -1,31 +1,29 @@
 import React from 'react';
 
-function ItemCount(stock,inicial){
-    const [counter, setCounter]= React.useState(inicial)
+function ItemCount({stock,inicial}) {
+    const [counter, setCounter]= React.useState({inicial})
     const sumador= () => {
          setCounter (counter+1)
     }
     const resta= () => {
         setCounter (counter-1)
     }
-    if (counter>=0 || counter<=stock ){
-        return(
-            <div className="itemCount">
-                <p>{counter}</p>
-                <button onClick= {sumador}> +1 </button>
-                <button onClick= {resta}>-1</button>
-            </div>
-        )
-        
-    }else{
-        return(
-            <div className="itemCount">
-                <p> STOCK AGOTAD0!</p>
-            </div>
-        )
+    const alert1= () => {
+        {alert("STOCK AGOTADO")}
     }
+    
+    const alert2= () => {
+        {alert("VALOR INVALIDO")}
+    }
+    
+
+    return(
+        <div className="itemCount">
+            <p>{counter}</p>
+            <button onClick= {() => (counter< {stock}? sumador: alert1)}> +1 </button>
+            <button onClick= {() => (counter> 0 ? resta: alert2 )} >-1</button>
+        </div>
+    )
    
 }
 export default ItemCount
-
-
